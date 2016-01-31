ae6816b9318485885c88c020e36b0b08fe372269
Roope Rajala <r_rajala@yahoo.com>
2016-01-31 11:26:32 +0200

    Create ritualists around circle based on count player count

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -34,7 +34,9 @@ public class GameController : MonoBehaviour {
 		WordGenerator.CreateWordPool();
 
 		RitualHandler.Instance.DistributeWords();
-        RitualHandler.Instance.NewRitual();	
+        RitualHandler.Instance.NewRitual();
+
+		CreateRitualists();
 	}
 
 	// Update is called once per frame
@@ -59,4 +61,47 @@ public class GameController : MonoBehaviour {
     void LoseGame() {
         Debug.Log("Game lost");
     }
+
+	void CreateRitualists() {
+		if(ServerNetworker.Instance == null) return;
+		int ritualistCount = ServerNetworker.Instance.connectedPlayers.Count;
+
+		List<float> rads = new List<float> { 0, Mathf.PI / 4, Mathf.PI / 2, 3/4 * Mathf.PI, 5/4 * Mathf.PI, 225, 3/2 * Mathf.PI, 7/4 * Mathf.PI };
+
+		Transform ritualistParent = GameObject.Find("SummoningCircle/Ritualists").transform;
+
+		GameObject firstRitualist = ritualistParent.GetChild(0).gameObject;
+
+		for(int i = 1; i < ritualistCount; i++) {
+			if(rads.Count == 0) break;
+
+			Transform newRitualist;
+
+			if(i < ritualistCount - 1) {
+				newRitualist = ((GameObject)Instantiate(firstRitualist)).transform;
+			} else {
+				newRitualist = firstRitualist.transform;
+			}
+
+			int rad = GameController.jamRandomer.Next(rads.Count);
+
+			newRitualist.position = Vector3.zero;
+
+			Vector3 spawnOffset = new Vector3(Mathf.Cos(rad),
+								  Mathf.Sin(rad),
+								  0f).normalized;
+
+			newRitualist.position += spawnOffset * 3f + new Vector3(0f, 1.5f, 0f);
+
+			newRitualist.parent = ritualistParent;
+			newRitualist.localScale = Vector3.one;
+
+			Vector3 pos = newRitualist.localPosition;
+			pos.z = 0f;
+
+			newRitualist.localPosition = pos;
+			
+			rads.Remove(rad);
+		}
+	}
 }
