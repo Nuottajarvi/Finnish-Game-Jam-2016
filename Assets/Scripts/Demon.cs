a73440f92cb4412a7a0d3aceeb42a417db045d0f
Urauth <pee-94@hotmail.com>
2016-01-31 10:59:22 +0200

    demon reset

--- Assets/Scripts/Demon.cs
+++ Assets/Scripts/Demon.cs
@@ -8,11 +8,11 @@ public class Demon : MonoBehaviour {
 	float time = 0f;
 
 	void Update(){
-		/*time+=Time.deltaTime;
+		time+=Time.deltaTime;
 		if(time > 2){
 			StartCoroutine("Raise");
 			time = -100000;
-		}*/
+		}
 	}
 
 	public IEnumerator Raise(){
@@ -41,5 +41,27 @@ public class Demon : MonoBehaviour {
 			spriteRenderer.color = Color.Lerp(spriteRenderer.color, new Color(1,1,1,1), Time.deltaTime * 2);
 			yield return new WaitForFixedUpdate();
 		}
+	}
+
+	public IEnumerator Reset(){
+		SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+		while(spriteRenderer.color.a > 1){
+			spriteRenderer.color = Color.Lerp(spriteRenderer.color, new Color(0,0,0,0), Time.deltaTime * 2);
+			yield return new WaitForFixedUpdate();
+		}
+
+		//CHANGE RING COLOR
+		Image healthbar = GameObject.Find("Bar").GetComponent<Image>();
+		healthbar.color = Color.cyan;
+
+
+		//CHANGE PARTICLE COLORS
+		ParticleSystem ringParticles = GameObject.Find("RitualCircleParticles").GetComponent<ParticleSystem>();
+		ringParticles.startColor = Color.cyan;
+
+		//DECREASE BLOOM
+		BloomOptimized bloom = GameObject.Find("Main Camera").GetComponent<BloomOptimized>();
+		bloom.intensity = 0.7f;
 	}
 }
